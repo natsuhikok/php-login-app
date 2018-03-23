13db34155e1680ef4e555fb3d0a111ae22396998
natsuhikok <kato@natsuhiko.jp>
2018-03-24 10:47:24 +1300

    Update: Clean

--- src/php/register.php
+++ src/php/register.php
@@ -1,18 +1,15 @@
 <?php
+ini_set( 'display_errors', 1 );
 // variables
-$page_title = 'Register | User Management Demo';
 $values['email'] = '';
 $values['password'] = '';
 $values['name'] = '';
-$validate_messages['email'] = '';
-$validate_messages['password'] = '';
-$validate_messages['name'] = '';
-$validation;
+$validate_messages = [];
+$validation = false;
 
 require_once('views/register_page.php');
 require_once('models/validation.php');
 require_once('models/add_user.php');
-
 
 // $_POST have something then validate
 if ($_POST) {
@@ -20,8 +17,8 @@ if ($_POST) {
 }
 
 // all validation is pass then add user into database
-if (isset($validation) && $validation) {
-  if(add_user($values)) {
+if ($validation) {
+  if (add_user($values)) {
     // add session[name & roll & message]
     $_SESSION['name'] = $values['name'];
     $_SESSION['message'] = 'wellcome, '.$values['name'].'!';
@@ -30,9 +27,12 @@ if (isset($validation) && $validation) {
     header("Location: home.php");
   }
 }
-// print page
-print_register_page($values, $validate_messages, $page_title);
 
-// unset session message
-print $_SESSION['message'];
-unset($_SESSION['message']);
+// print page
+print_register_page($values, $validate_messages);
+
+// print and unset session message
+if (isset($_SESSION['message'])) {
+  print $_SESSION['message'];
+  unset($_SESSION['message']);
+}
--- src/php/views/register_page.php
+++ src/php/views/register_page.php
@@ -1,14 +1,47 @@
 <?php
-require_once('views/form.php');
 require_once('views/head.php');
-function print_register_page($values, $messages, $title) {
+function print_register_page($values, $messages) {
+  // page title
+  $title = 'Register | User Management Demo';
+  // values
+  $email_value = $values['email'];
+  $pwd_value = $values['password'];
+  $name_value = $values['name'];
+
+  // messages
+  $email_msg = isset($messages['email']) ? $messages['email'] : '';
+  $pwd_msg = isset($messages['password']) ? $messages['password'] : '';
+  $name_msg = isset($messages['name']) ? $messages['name'] : '';
+
+  // input class
+  $email_css = empty($email_msg) ? '' : 'error';
+  $pwd_css = empty($pwd_msg) ? '' : 'error';
+  $name_css = empty($name_msg) ? '' : 'error';
 ?>
 
 <html>
   <?php print_head($title); ?>
   <body>
     <h1>Register</h1>
-    <?php print_full_form($values, $messages); ?>
+    <form action method="post">
+      <div>
+        <label for="email">
+          Email
+          <span><?php echo $email_msg; ?></span>
+        </label>
+        <input class="<?php echo $email_css; ?>" type="text" name="email" value="<?php echo $email_value; ?>">
+      </div>
+      <div>
+        <label for="password">Password<span><?php echo $pwd_msg; ?></span></label>
+        <input class="<?php echo $pwd_css; ?>" id="password" type="password" name="password" value="<?php echo $pwd_value; ?>">
+      </div>
+      <div>
+        <label for="name">Name<span><?php echo $name_msg; ?></span></label>
+        <input class="<?php echo $name_css; ?>" id="name" type="text" name="name" value="<?php echo $name_value; ?>">
+      </div>
+      <input type="submit" value="sign up" name="signup">
+    </form>
+    <a href="./">login</a>
   </body>
 </html>
 
