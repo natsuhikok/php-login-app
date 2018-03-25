fbb529b3d5f5a63f2bab390154426054c82cb14c
natsuhikok <kato@natsuhiko.jp>
2018-03-26 07:50:13 +1300

    Add: function to update user password

--- src/php/models/update_user.php
+++ src/php/models/update_user.php
@@ -20,4 +20,25 @@ function update_user_info($values, $ID) {
     return false;
   }
 }
-?>
+
+function update_user_password($new_password, $ID) {
+  // connect or return
+  require_once('db/connect.php');
+  $db = connect();
+  if ($db -> connect_error) return;
+
+  // encrypt password
+  $PASSWORD = password_hash($new_password, PASSWORD_DEFAULT);
+
+  // define insert data and SQL
+  $sql = "UPDATE user SET PASSWORD = '$PASSWORD' WHERE ID = '$ID';";
+
+  // execute !!!
+  if ($db -> query($sql)) {
+    return true;
+  } else {
+    // if error while execution, add error message
+    $_SESSION['message'] = $db -> error;
+    return false;
+  }
+}
