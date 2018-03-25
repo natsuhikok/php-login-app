5ebb532e24464a5a51821c82a5ed977d6aa90df3
natsuhikok <kato@natsuhiko.jp>
2018-03-26 07:56:46 +1300

    Add: password validations

--- src/php/models/validation.php
+++ src/php/models/validation.php
@@ -20,13 +20,20 @@ function validation($values) {
     // genarate validate messages
     switch ($key) {
     case 'email':
-        $messages[$key] = validate_email();
+        $messages[$key] = validate_email($values[$key]);
         break;
     case 'password':
-        $messages[$key] = validate_password();
+        $messages[$key] = validate_password($values[$key]);
+        break;
+    case 'old_password':
+        $messages[$key] = validate_password($values[$key]);
+        $messages[$key] = check_password($values[$key]);
+        break;
+    case 'new_password':
+        $messages[$key] = validate_password($values[$key]);
         break;
     case 'name':
-        $messages[$key] = validate_name();
+        $messages[$key] = validate_name($values[$key]);
         break;
     }
   }
@@ -40,20 +47,41 @@ function validation($values) {
 
 
 
-function validate_email() {
-  if (strlen($_POST['email']) < 3) {
+function validate_email($v) {
+  if (strlen($v) < 3) {
     return 'enter vaild email';
   }
 }
 
-function validate_password() {
-  if (strlen($_POST['password']) < 6) {
+function validate_password($v) {
+  if (strlen($v) < 6) {
     return 'password should be atleast 6 characters.';
   }
 }
 
-function validate_name() {
-  if (strlen($_POST['name']) < 2) {
+function validate_name($v) {
+  if (strlen($v) < 2) {
     return 'name should be atleast 2 characters.';
   }
+}
+
+require_once('db/connect.php');
+function check_password($v) {
+  // connect or return
+  $db = connect();
+  if ($db -> connect_error) return 'server error.';
+
+  $ID = $_SESSION['editing_id'];
+  // find email from database
+  $sql = "SELECT * FROM user WHERE ID = '$ID'";
+
+  if ($result = $db -> query($sql)) {
+    if ($row = $result -> fetch_assoc()) {
+      // verify password
+      if (password_verify($v, $row['PASSWORD'])) {
+        return;
+      }
+    }
+  }
+  return 'passoword is not correct.';
 }
