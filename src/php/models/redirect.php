dd5c739bac9d4f5673fa7873a230793e7b3ebc45
natsuhikok <kato@natsuhiko.jp>
2018-03-25 22:30:43 +1300

    Update: comments

--- src/php/models/redirect.php
+++ src/php/models/redirect.php
@@ -2,18 +2,21 @@
 // rolls none/user/admin
 function redirect_if_not($roll) {
   switch ($roll) {
+    // index, register
     case 'none':
       if (isset($_SESSION['roll'])) {
         header("Location: home.php");
         exit;
       }
       break;
+    // home, edituser
     case 'user':
       if (!isset($_SESSION['roll'])) {
         header("Location: index.php");
         exit;
       }
       break;
+    // admin
     case 'admin':
       if (!isset($_SESSION['roll'])) {
         header("Location: index.php");
