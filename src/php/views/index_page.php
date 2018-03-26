daaad75d1ec129af451855c2889b61d20a8e5a53
natsuhikok <kato@natsuhiko.jp>
2018-03-27 06:39:50 +1300

    Add: login style

--- src/php/views/index_page.php
+++ src/php/views/index_page.php
@@ -2,6 +2,7 @@
 function print_index_page($values, $messages) {
   // page title
   require_once('views/print_head.php');
+  require_once('views/print_form_email.php');
   $title = 'Login | PHP LOGIN APP';
 
   // values
@@ -13,28 +14,30 @@ function print_index_page($values, $messages) {
   $pwd_msg = isset($messages['password']) ? $messages['password'] : '';
 
   // input class
-  $email_css = empty($email_msg) ? '' : 'error';
   $pwd_css = empty($pwd_msg) ? '' : 'error';
 ?>
 <html>
   <?php print_head($title); ?>
   <body>
-    <h1>PHP LOGIN APP</h1>
-    <form action method="post">
-      <div>
-        <label for="email">
-          Email
-          <span><?php echo $email_msg; ?></span>
-        </label>
-        <input class="<?php echo $email_css; ?>" type="text" name="email" value="<?php echo $email_value; ?>">
+    <div class="l-wrapper loginBackground">
+      <div class="loginBox">
+        <h1 class="loginBox--headline">PHP LOGIN APP</h1>
+        <form class="loginBox--form" action method="post">
+          <?php print_form_email($email_value, $email_msg); ?>
+          <div class="input">
+            <label class="input--label" for="password">Password<span><?php echo $pwd_msg; ?></span></label>
+            <input class="<?php echo $pwd_css; ?>" id="password" type="password" name="password" value="<?php echo $pwd_value; ?>">
+          </div>
+          <div class="loginBox--form--btn">
+            <input class="btn" type="submit" value="sign in" name="signin">
+          </div>
+        </form>
+        <div class="loginBox--link">
+          <a href="./register.php">register</a>
+        </div>
       </div>
-      <div>
-        <label for="password">Password<span><?php echo $pwd_msg; ?></span></label>
-        <input class="<?php echo $pwd_css; ?>" id="password" type="password" name="password" value="<?php echo $pwd_value; ?>">
-      </div>
-      <input type="submit" value="sign in" name="signin">
-    </form>
-    <a href="./register.php">register</a>
+      <?php include('views/print_footer.php'); ?>
+    </div>
   </body>
 </html>
 
